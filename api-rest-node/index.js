3d677dce7104a7611c2381129acd8fdac2141691
ing-javier <devjavrn@gmail.com>
2026-04-27 18:38:27 -0500

    se crea primer archivo principal index

--- api-rest-node/index.js
+++ api-rest-node/index.js
@@ -1,5 +1,24 @@
-const  { conection } = require("./database/conection.js")
-
+const  { conection } = require("./database/conection.js");
+const  express = require("express");
+const cors = require("cors")
 
 // conectar a la db
 conection();
+
+// Crear servidor de node
+const app = express();
+const puerto = 3900;
+
+// Configurar cors
+app.use(cors());
+
+// Convertir body a objeto js
+app.use(express.json());
+
+// Crear rutas
+
+
+// Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor corriendo en el puerto "+ puerto)
+});
